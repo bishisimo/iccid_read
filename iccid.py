ede63d271f9aaf917230e7e4ebf961d63d47f9ac
bishisimo <bishisimo@qq.com>
2019-06-22 18:55:33 +0800

    修改为内网ip,修改为命令行参数模式

--- iccid.py
+++ iccid.py
@@ -1,3 +1,4 @@
+import argparse
 from datetime import datetime
 
 import redis
@@ -5,6 +6,28 @@ from redis import ConnectionPool
 import time
 import xlwt
 
+def get_args():
+    result={"hour":0,"min":0} #使用一个字典以存储参数
+    parser = argparse.ArgumentParser(description="your script description") #实例化一个参数对象,描述参数为help内容
+    parser.add_argument('--year', type=int)  # 创建一个--hour参数,接收数据类型为int
+    parser.add_argument('--month', type=int)  # 创建一个--min参数,接收数据类型为int
+    parser.add_argument('--day', type=int)  # 创建一个--hour参数,接收数据类型为int
+    parser.add_argument('--hour', type=int) #创建一个--hour参数,接收数据类型为int
+    parser.add_argument('--min', type=int) #创建一个--min参数,接收数据类型为int
+    args=parser.parse_args() #获得参数对象
+    arg_dic={}
+    if args.year is not None:
+        arg_dic['year']=args.year
+    if args.month is not None:
+        arg_dic['month']=args.month
+    if args.day is not None:
+        arg_dic['day']=args.day
+    if args.hour is not None:
+        arg_dic['hour']=args.hour
+    if args.min is not None:
+        arg_dic['min']=args.min
+
+    return arg_dic
 
 def get_timestamp(year=2019, month=6, day=10, hour=0, min=0, sec=0):
     now = datetime(year, month, day, hour, min, sec)
@@ -29,10 +52,9 @@ def save2excel(data: dict):
 
 def app(target_timestamp):
     # host = '118.89.106.236'
-    host = '39.98.164.160'
+    host = '127.0.0.1'
     port = 6379
-    # pool = ConnectionPool(host=host, port=port, password='bishisimo', decode_responses=True)
-    pool = ConnectionPool(host=host, port=port,  decode_responses=True)
+    pool = ConnectionPool(host=host, port=port, password='bishisimo', decode_responses=True)
     r = redis.StrictRedis(connection_pool=pool)
     t = r.dbsize()
     i = 0
@@ -52,5 +74,7 @@ def app(target_timestamp):
     save2excel(data)
 
 if __name__ == '__main__':
-    target_timestamp = get_timestamp()
+    arg_dic=get_args()
+    target_timestamp = get_timestamp(**arg_dic)
+    print(target_timestamp)
     app(target_timestamp)
